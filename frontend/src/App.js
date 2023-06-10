e838f6208c9809da2804820fbc85a99d2a291a32
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-06-10 17:54:11 +0530

    Modified something;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import React from 'react';
 import axios from 'axios';
-import env from 'react-dotenv';
+import env from "dotenv";
+
+env.config();
 
 const App = () => {
   const [ip, setIp] = React.useState(null);
   const [msg, setMsg] = React.useState('');
 
-  const PORT = env?.BE_PORT || 5000;
-  const HOST = env?.BE_HOST || "localhost";
+  const PORT = env?.REACT_APP_BE_PORT || 5000;
+  const HOST = env?.REACT_APP_BE_HOST || "localhost";
 
   console.log("PORT: ", PORT);
   console.log("HOST: ", HOST);
-  console.log("env: ", env.BE_HOST, env.BE_PORT);
+  console.log("env: ", process.env.REACT_APP_BE_HOST, process.env.REACT_APP_BE_PORT);
 
   React.useEffect(() => {
     axios.get(`http://${HOST}:${PORT}`)
